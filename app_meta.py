6850940884a32f28e34c904435bb1325a967343f
serrebi <brandon@bracey.ca>
2025-12-30 20:55:20 -0800

    chore(release): v1.93.3

--- app_meta.py
+++ app_meta.py
@@ -1,6 +1,6 @@
 APP_NAME = "AccessibleIPTVClient"
 APP_DISPLAY_NAME = "Accessible IPTV Client"
-APP_VERSION = "1.93.2"
+APP_VERSION = "1.93.3"
 EXE_NAME = "IPTVClient.exe"
 GITHUB_OWNER = "serrebi"
 GITHUB_REPO = "Accessible-IPTV-Client"
