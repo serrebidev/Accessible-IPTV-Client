4fcc5fc98fc7f0d230ec5c59638e7b3048479e6b
admin <admin@local>
2026-01-31 19:31:45 -0800

    chore(release): v1.94.1

--- app_meta.py
+++ app_meta.py
@@ -1,6 +1,6 @@
 APP_NAME = "AccessibleIPTVClient"
 APP_DISPLAY_NAME = "Accessible IPTV Client"
-APP_VERSION = "1.94.0"
+APP_VERSION = "1.94.1"
 EXE_NAME = "IPTVClient.exe"
 GITHUB_OWNER = "serrebi"
 GITHUB_REPO = "Accessible-IPTV-Client"
